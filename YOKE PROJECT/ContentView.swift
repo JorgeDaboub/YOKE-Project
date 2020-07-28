c4de052f832ece9a672c0fccbb730a59f3edd39d
Jorge <jdaboub@nd.edu>
2020-07-28 17:03:30 -0600

    Initial Commit

---
+++ YOKE PROJECT/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  YOKE PROJECT
+//
+//  Created by Jorge Daboub on 7/28/20.
+//  Copyright © 2020 Jorge Daboub. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
